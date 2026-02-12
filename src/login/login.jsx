6e4d5a39e1df5fd6ea977376123ac827b235ca68
Stephen <deathanddoom23@gmail.com>
2026-02-11 23:23:03 -0700

    Converted the login to react

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import './login.css';
+
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+      <main>
+        <div className="index-container d-flex flex-column align-items-center">
+            <h1>Enter the Second Monitor!</h1>
+            <form method="get" action="play.html">
+                <div className="mb-3">
+                    <label class="form-label">Character Name</label>
+                    <input type="text" className="form-control" placeholder="Scrungly the Bold" />
+                </div>
+
+                <div className="mb-3">
+                    <label className="form-label">Password</label>
+                    <input type="password" className="form-control" placeholder="password" />
+                </div>
+
+                <div className="form-buttons">
+                    <button type="submit" className="btn btn-rpg">Login</button>
+                    <button type="submit" className="btn btn-secondary">Create</button>
+                </div>
+            </form>
+        </div>
     </main>
   );
 }

5afe7d98d7617f557b0de3a3fe382f45df8cb2b1
Stephen <deathanddoom23@gmail.com>
2026-02-11 23:26:51 -0700

    Ported play to react

--- src/play/play.jsx
+++ src/play/play.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
 
+import './play.css';
+
 export function Play() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>play displayed here</div>
+    <main>
+
+        <p id="experience">
+            Experience: <strong>120</strong>
+        </p>
+
+        <h1 id="encounter-title">Encounter</h1>
+
+        {/* Enemy Display */}
+        <section id="enemy">
+
+            <h2 id="enemy-name">Goblin Placeholder</h2>
+
+            {/* Enemy visual container */}
+            <div id="enemy-visual">
+
+                <img
+                    src="placeholder-enemy.png"
+                    alt="Enemy placeholder image"
+                />
+
+                {/* Damage numbers will be injected here later */}
+            </div>
+
+            {/* Enemy Health */}
+            <p>Health</p>
+            <progress 
+                id="enemy-health"
+                value="75" 
+                max="100">
+            </progress>
+            <p>75 / 100</p>
+
+        </section>
+        <div id="actions" class="d-flex gap-2 mt-3">
+            <button class="btn btn-rpg">Attack</button>
+            <button class="btn btn-secondary">Defend</button>
+        </div>
+
     </main>
   );
 }

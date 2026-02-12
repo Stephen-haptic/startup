131afbff8e99e555a384a897abbc918c4890c28c
Stephen <deathanddoom23@gmail.com>
2026-02-11 23:10:06 -0700

    Created view components and renamed index.css to login.css

---
+++ src/character/character.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Character() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>character displayed here</div>
+    </main>
+  );
+}
---
+++ src/raid/raid.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Raid() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>raid displayed here</div>
+    </main>
+  );
+}
